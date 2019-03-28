8966bbbe73a059567f3bb62b441b95a53c02b97b
root <kuznec3093@gmail.com>
2019-03-28 19:00:52 +0300

    last change

--- main.cpp
+++ main.cpp
@@ -1,12 +1,3 @@
-//
-// blocking_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -75,7 +66,7 @@ int main(void)
     {
         /* worked! */
     }
-   
+
 
 
     char buf[MAXBUFFERSIZE];
