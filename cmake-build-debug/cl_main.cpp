4fb64878b31b931f16e67ab2ff9ce4b10ed8b3ea
root <kuznec3093@gmail.com>
2019-03-29 03:52:10 +0300

    added full map sending and recieving

--- cmake-build-debug/cl_main.cpp
+++ cmake-build-debug/cl_main.cpp
@@ -11,11 +11,16 @@
 #include <arpa/inet.h>
 #include <fstream>
 #include <ostream>
+#include <vector>
+#include <map>
 #include <istream>
 #include "TwoDimArray.h"
+#include <libtcod.hpp>
 using std::ifstream;
 using std::istream;
 using std::string;
+using std::vector;
+using std::map;
 
 
 #define PORT "50001" // the port client will be connecting to
@@ -23,6 +28,49 @@ using std::string;
 
 //istream buffer;
 char buf[MAXBUFFERSIZE];
+
+void PaintOfScreen(TwoDimArray<char>& TwoDArray, const vector<TCODColor>& colVec, vector<int>& tmpPlrPos, map<vector<int>,char>& mapOfWinPositions){
+    TCODConsole::initRoot(TwoDArray.getDimX(),TwoDArray.getDimY(),"AlexSmith's Sokoban", false, TCOD_RENDERER_GLSL);
+    TCODConsole::root->setDefaultBackground({0,0,0});
+
+    for (auto i = 0; i < TwoDArray.getDimY(); i++){
+        for (auto j = 0; j < TwoDArray.getDimX(); j++){
+            TCODConsole::root -> setChar(j, i, TwoDArray.getObjPos(i,j));
+            if (TwoDArray.getObjPos(i,j) == 'P'){
+                TCODConsole::root->setCharBackground(j,i,colVec[0]);
+            }
+            else if (TwoDArray.getObjPos(i,j) == '#'){
+                TCODConsole::root->setCharBackground(j,i,colVec[1]);
+            }
+            else if (TwoDArray.getObjPos(i,j) == 'B'){
+                TCODConsole::root->setCharBackground(j,i,colVec[2]);
+            }
+            else if (TwoDArray.getObjPos(i,j) == 'X'){
+                TCODConsole::root->setCharBackground(j,i,colVec[3]);
+            }
+        }
+    }
+
+    /*for (auto i = 0; i < TwoDArray.getDimY(); i++){
+        for (auto j = 0; j < TwoDArray.getDimX(); j++){
+
+            if (TCODConsole::root -> getChar(j, i) == 'P'){
+                TCODConsole::root->setCharBackground(j,i,colVec[0]);
+            }
+            else if (TCODConsole::root -> getChar(j, i) == '#'){
+                TCODConsole::root->setCharBackground(j,i,colVec[1]);
+            }
+            else if (TCODConsole::root -> getChar(j, i) == 'B'){
+                TCODConsole::root->setCharBackground(j,i,colVec[2]);
+            }
+            else if (TCODConsole::root -> getChar(j, i) == 'X'){
+                TCODConsole::root->setCharBackground(j,i,colVec[3]);
+            }
+        }
+    }*/
+    TCODConsole::root->flush();
+}
+
 
 // get appropriate sockaddress, IPv4 or IPv6:
 void *get_approp_addr(struct sockaddr *sock_a);
@@ -36,8 +84,17 @@ struct membuf : std::streambuf
 
 int main(int argc, char *argv[])
 {
-    TwoDimArray<char> Test; //create instance of 2DArray class
+    const TCODColor player {0,255,0};
+    const TCODColor wall{255,0,0};
+    const TCODColor box{255,255,0};
+    const TCODColor winCross{255,255,255};
+    const vector<TCODColor> colourVec = {player, wall, box, winCross}; //0 - player, 1 - wall, 2 - box, 3 - winCross
+    vector<int> tempPlrPos; //temporary player position vector (x,y)
+    map<vector<int>,char> mapCharWin;
 
+
+    TwoDimArray<char> Test; //create instance of 2DArray class
+    bool isMapRecieved = false;
     int socket_desc, numbytes;
 
     struct addrinfo hints, *list_of_server_info, *p;
@@ -87,12 +144,14 @@ int main(int argc, char *argv[])
         perror("recv");
         exit(1);
     }
-    //std::stringstream ss(std::string(buf));
-
-    membuf str_buf(buf, buf + sizeof(buf));
-    std::istream in(&str_buf);
-    in >> Test;
-    std::cout << Test;
+    if (ch == 'm' && !isMapRecieved) {
+        membuf str_buf(buf, buf + sizeof(buf));
+        std::istream in(&str_buf);
+        in >> Test;
+        //std::cout << Test;
+        PaintOfScreen(Test, colourVec, tempPlrPos, mapCharWin);
+        isMapRecieved = true;
+    }
 
     close(socket_desc);
     //printf("client: received from server echo'%s'\n",buf);
